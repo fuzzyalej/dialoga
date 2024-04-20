83f2b0544adcbba7fd93d8c551bdf3d7f0f9674c
Alejandro Andres Rodriguez <alejandro.andres@coches.com>
2024-04-20 22:23:21 +0200

    title...

--- src/utils/all.js
+++ src/utils/all.js
@@ -1,7 +1,7 @@
 /** */
 export const getFormattedDate = (date) =>
   date
-    ? new Date(date).toLocaleDateString("en-us", {
+    ? new Date(date).toLocaleDateString("es-es", {
         year: "numeric",
         month: "short",
         day: "numeric",
